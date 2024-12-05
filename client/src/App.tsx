3197c2e5348ad0319f3d6cbad80bdc6c18b4c607
Faccinetto <bastien.faccinetto@gmail.com>
2024-12-05 14:35:19 +0100

    Header début

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import img from "./assets/images/sau1teur.gif";
+import Header from "./components/header.tsx";
 
 function App() {
 	return (
 		<>
+			<header>
+				<Header />
+			</header>
 			<script
 				async
 				src="https://cse.google.com/cse.js?cx=b2615d1ca9908476e"
@@ -13,7 +17,7 @@ function App() {
 				{/* //ne marche pas pour l'instant */}
 				<h2></h2>
 			</div>
-			<footer>
+			{/* <footer>
 				<p className="liens_footer">
 					<a href="https://myspace.com/">myspace </a>
 					<a href="https://myspace.com/"> skyblog </a>
@@ -22,7 +26,7 @@ function App() {
 					<a href="https://myspace.com/"> wanadoo </a>
 					<a href="https://myspace.com/"> msn</a>
 				</p>
-			</footer>
+			</footer> */}
 		</>
 	);
 }
