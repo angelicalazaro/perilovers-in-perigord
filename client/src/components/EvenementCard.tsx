9e462222ccba450cff6ec517549be5af0983cf11
Faccinetto <bastien.faccinetto@gmail.com>
2024-12-05 22:25:33 +0100

    résolution bugs

--- client/src/components/EvenementCard.tsx
+++ client/src/components/EvenementCard.tsx
@@ -9,33 +9,18 @@ function EvenementCard({ evenement }: EvenementCardProps) {
 	const [inscrit, setInscrit] = useState(false);
 
 	return (
-<<<<<<< HEAD
-		<figure className="card">
-			{" "}
-			{/* Appliquez uniquement la classe 'card' ici */}
-			<img src={evenement.picture} alt={evenement.name} />
-			<h3>{evenement.name}</h3>
-			<p>
-				{evenement.description} <br />
-				Lieu {evenement.lieu} <br />
-				{evenement.date}
-			</p>
-			<button className="bouttonInscrit" onClick={() => setInscrit(!inscrit)}>
-				{inscrit ? "Je suis inscrit(e)" : "Je m'inscris"}
-			</button>
-		</figure>
-=======
 		<>
 			<div className="card-container">
 				<figure className="card">
-					<img src={evenement.picture} />
+					<img src={evenement.picture} alt="TG" />
 					<h3>{evenement.name}</h3>
 					<p>
 						{evenement.description} <br />
-						Lieu : {evenement.lieu} <br />
-						Date : {evenement.date}
+						Lieu {evenement.lieu}
+						{evenement.date}
 					</p>{" "}
 					<button
+						type="button"
 						className="bouttonInscrit"
 						onClick={() => setInscrit(!inscrit)}
 					>
@@ -44,7 +29,6 @@ function EvenementCard({ evenement }: EvenementCardProps) {
 				</figure>
 			</div>
 		</>
->>>>>>> ec3f3fcb9d3fb45a370d7d2b002dfe7cbf2f7db4
 	);
 }
 
