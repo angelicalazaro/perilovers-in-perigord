bb93431723778e46c08a6666b4cb793fc5c5b559
Lise PERARD <liseprd@hotmail.fr>
2024-12-05 13:09:32 +0100

    un début

---
+++ client/src/components/header.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function Header() {
+	return (
+		<>
+			<header>
+				<img src="" alt="balec de l'alt on est dans les années 2000" />
+				<h1>N°1 des sites de rencontres pour Perigordins et Perigordines</h1>
+			</header>
+
+			<main></main>
+		</>
+	);
+}
+
+export default Header;
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function Home() {
+	return (
+		<>
+			<button>Se déconnecter</button>
+			<button>Vous êtes le xème visiteur de notre site !</button>
+
+			<main></main>
+		</>
+	);
+}
+
+export default Home;
